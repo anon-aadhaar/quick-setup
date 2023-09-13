f4dbc7ca643ee92403833e6b5ab8fa8b9b327865
Yanis Meziane <yanis.meziane@essec.edu>
2023-09-13 12:44:29 +0200

    anon aadhaar integrated

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  webpack: (config) => {
+    config.resolve.fallback = {
+      fs: false,
+      readline: false,
+    };
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
